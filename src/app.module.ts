912f11d411dbfa4a8f9b4a6a35e2a12356be32a6
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-02-23 22:13:49 +0100

    chore: add docker-compose for local dev & configure typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        if (process.env.NODE_ENV === 'production') {
+          return {
+            type: 'postgres',
+            url: configService.get<string>('DATABASE_URL'),
+            synchronize: false,
+            autoLoadEntities: true,
+          };
+        }
+        return {
+          type: 'postgres',
+          host: configService.get<string>('PGHOST'),
+          port: configService.get<number>('PGPORT'),
+          username: configService.get<string>('PGUSER'),
+          password: configService.get<string>('PGPASSWORD'),
+          database: configService.get<string>('PGDATABASE'),
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
